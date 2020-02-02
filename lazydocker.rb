3d6868a3e1dae7cb299f9355ec403923a55780b6
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-02 11:23:10 +1100

    Brew formula update for lazydocker version v0.8

--- lazydocker.rb
+++ lazydocker.rb
@@ -2,24 +2,24 @@
 class Lazydocker < Formula
   desc "A simple terminal UI for docker, written in Go"
   homepage "https://github.com/jesseduffield/lazydocker/"
-  version "0.7.6"
+  version "0.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/jesseduffield/lazydocker/releases/download/v0.7.6/lazydocker_0.7.6_Darwin_x86_64.tar.gz"
-    sha256 "8321d4cd49eb6d3117990542b0bebb21659f2cdda601813508799c4c33be8f2d"
+    url "https://github.com/jesseduffield/lazydocker/releases/download/v0.8/lazydocker_0.8_Darwin_x86_64.tar.gz"
+    sha256 "088e8cdcdca15e40267480a7271c782ac395f1f382d43c560d6f82b63383d535"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/jesseduffield/lazydocker/releases/download/v0.7.6/lazydocker_0.7.6_Linux_x86_64.tar.gz"
-      sha256 "71081c3bd6bc1b9b750c776b6a10b2716ffa924b66829fcf44f6fbdab6975417"
+      url "https://github.com/jesseduffield/lazydocker/releases/download/v0.8/lazydocker_0.8_Linux_x86_64.tar.gz"
+      sha256 "3cb328239d414cb91d907c8bf93ab0ab1a9394181e24cd7153f8f29277e46919"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jesseduffield/lazydocker/releases/download/v0.7.6/lazydocker_0.7.6_Linux_arm64.tar.gz"
-        sha256 "530dbc3beca1c24398169b1465c41b1618d78e67b2326de1bc46669f97b561cf"
+        url "https://github.com/jesseduffield/lazydocker/releases/download/v0.8/lazydocker_0.8_Linux_arm64.tar.gz"
+        sha256 "e22bed58703ee9362ba861861724396faab444faaaf8a50f4ddbd2a886222353"
       else
-        url "https://github.com/jesseduffield/lazydocker/releases/download/v0.7.6/lazydocker_0.7.6_Linux_armv6.tar.gz"
-        sha256 "94335a3addb8acc2a5f3551366abbfd3f8d60b3f22a14355fc8da75e165a47a7"
+        url "https://github.com/jesseduffield/lazydocker/releases/download/v0.8/lazydocker_0.8_Linux_armv6.tar.gz"
+        sha256 "08a19a5a1236d6646fc143d77bad37163a957eeddafb5480e667fbb51f82b674"
       end
     end
   end
